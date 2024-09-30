83f212cb8e677b526371af0682a6850cb562b34e
Michele Papucci <m.papucci@studenti.unipi.it>
2024-09-30 17:12:36 +0200

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: michelepapucci.github.io,
+  site: "michelepapucci.github.io",
   integrations: [mdx(), sitemap(), tailwind()]
 });
