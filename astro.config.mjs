4688fa6f485a46e8d6994d9aa35b2e1150c67c81
Michele Papucci <m.papucci@studenti.unipi.it>
2024-09-30 16:51:57 +0200

    deploy with github actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://astrofy-template.netlify.app',
+  site: michelepapucci.github.io,
+  base: 'michelepapucci.github.io',
   integrations: [mdx(), sitemap(), tailwind()]
 });
