4d969b8949a582517f1f3eb72bc07705781a2671
Michele Papucci <m.papucci@studenti.unipi.it>
2024-10-02 14:15:52 +0200

    fixed rss feed

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,8 +1,12 @@
 import rss from "@astrojs/rss";
 import { SITE_TITLE, SITE_DESCRIPTION } from "../config";
 import { getCollection } from "astro:content";
+import sanitizeHtml from 'sanitize-html';
+import MarkdownIt from 'markdown-it';
+const parser = new MarkdownIt();
 
-export async function get(context) {
+
+export async function GET(context) {
   const blog = await getCollection("blog");
   return rss({
     title: SITE_TITLE,
@@ -13,6 +17,9 @@ export async function get(context) {
       pubDate: post.data.pubDate,
       description: post.data.description,
       link: `/blog/${post.slug}/`,
+      content: sanitizeHtml(parser.render(post.body), {
+        allowedTags: sanitizeHtml.defaults.allowedTags.concat(['img'])
+      }),
     })),
   });
 }
